ad08152fd7626fcc986fb7945828fc0a131586ae
Eles-Balazs-Mark <balazsmarkeles@gmail.com>
2026-09-14 11:27:09 +0200

    js demo

---
+++ script.js
@@ -0,0 +1,4 @@
+const test = document.getElementById("demo");
+const recipeBlock = document.getElementById("recipe"); 
+const recipeIngredients = document.getElementById("ingredient");
+//kell Id neveket kitalálni, de később még változtathatunk rajta
