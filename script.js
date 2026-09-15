050390ba3ea0d5d67ea6a8bbb5e6673472c01c44
Eles-Balazs-Mark <balazsmarkeles@gmail.com>
2026-09-15 09:38:14 +0200

    pages + opening js for them

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const test = document.getElementById("demo");
 const div = document.getElementById("root");
 const recipeBlock = document.getElementById("recipe"); 
 const recipeIngredients = document.getElementById("ingredient");
 const navBar = document.getElementById("navBar");
 const searchBar = document.getElementById("searchBar");
 const menu = document.getElementById("menu");
-const recipeBook = document.getElementById("recipeBook");
+const recipes = document.getElementById("recipes");
+const cooking  = document.getElementById("cooking");
+const account = document.getElementById("account");
 const shoppinglist = document.getElementById("shoppinglist");
 //kell Id neveket kitalálni, de később még változtathatunk rajta
+
+//bevásárlólista oldalának nyitása
 function shopping(){
-    /*open("index/shopping.html")*/
+    open("shopping.html", "_self")
     console.log("okay👍")
 }
+//nemtudom hogy minek az oldalának nyitása
+function idkmegnemdontottemel(){
+    open("idkmegnemdontottemel.html", "_self")
+    console.log("okay👍")
+}
+//receptek oldalának nyitása
+function recipebook(){
+    open("recipes.html", "_self")
+    console.log("okay👍")
+}
+//profil oldalának nyitása
+function profile(){
+    open("account.html", "_self")
+    console.log("okay👍")
+}
+//oldal betöltéséért felelős függvény
+//elvileg
 function load(){
     /**/
 }
 shoppinglist.addEventListener("click", shopping)
+recipes.addEventListener("click", recipebook)
+cooking.addEventListener("click", idkmegnemdontottemel)
+account.addEventListener("click", profile)
